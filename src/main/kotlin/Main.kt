ec2056980441835325a0e6a891bbb3c51b0e3b97
boscoFung <bosco0283000@gmail.com>
2025-03-17 23:31:05 +0800

    lab7 version

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val totalParticipants = 4 // Example: can be adjusted
+    val lordFactory = LordFactory()
+    val lord = lordFactory.createRandomGeneral()
+    if (lord != null) {
+        GeneralManager.addPlayer(LiuBei().apply { strategy = LordStrategy() })
+    }
+
+    val nonLordFactory = NonLordFactory()
+    val nonLordPlayers = nonLordFactory.createNonLordPlayers(totalParticipants)
+    nonLordPlayers.forEach { GeneralManager.addPlayer(it) }
+    val caoCaoPlayer = GeneralManager.getPlayerList().find { it is CaoCao } as? CaoCao
+    if (caoCaoPlayer != null) {
+        println("caocao is here")
+        val factory = NonLordFactory()
+        val weiChainHead = factory.buildWeiChain(null, GeneralManager.getPlayerList())
+        caoCaoPlayer.next = weiChainHead
+        factory.printWeiChainMessages()
+    }
+
+    val lordPlayer = GeneralManager.getPlayerList().find { it is General && it.strategy is LordStrategy } as? General
+    if (lordPlayer != null) {
+        val spies = GeneralManager.getPlayerList().filter { it is General && it.strategy is SpyStrategy }
+        for (spy in spies) {
+            lordPlayer.registerObserver(SpyObserver(spy as General))
+        }
+    }
+
+    GeneralManager.gameStart()
+}
