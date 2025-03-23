fed01009d097725b08f84b2051c0a432eb8212bb
Shamanliu174 <meteork1101@gmail.com>
2025-03-23 19:36:22 +0800

    Clear CardDeck

--- src/main/kotlin/Card/CardDeck.kt
+++ src/main/kotlin/Card/CardDeck.kt
@@ -138,7 +138,7 @@ object CardDeck {
 
     fun discardCard(card: Card) {
         discardPile.add(card) // 將卡牌加入棄牌堆
-        println("====- DiscardPile SIZE: ${discardPile.size}, Discarded ${card.toString()} -====")
+        println("= DiscardPile SIZE: ${discardPile.size}, Discarded ${card.toString()} =")
     }
 
     fun printCard() { //看牌庫
